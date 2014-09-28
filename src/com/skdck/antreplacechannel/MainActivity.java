504b8697900b60d076293a08a39c2aa1282026f3
skdck <skdck@163.com>
2014-09-28 18:45:35 +0800

    修改build.xml使之能够多渠道批量打包，修改MainActivity显示渠道信息

--- src/com/skdck/antreplacechannel/MainActivity.java
+++ src/com/skdck/antreplacechannel/MainActivity.java
@@ -1,9 +1,11 @@
 package com.skdck.antreplacechannel;
 
 import android.app.Activity;
+import android.content.ComponentName;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -11,5 +13,22 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+
+		((TextView) findViewById(R.id.tv)).setText("CHANNEL:"
+				+ getMetaData("CHANNEL") + ",NAME:" + getMetaData("NAME"));
+	}
+
+	private String getMetaData(String name) {
+		try {
+			return getPackageManager()
+					.getApplicationInfo(
+							new ComponentName(this, MainActivity.class)
+									.getPackageName(),
+							PackageManager.GET_META_DATA).metaData
+					.getString(name);
+		} catch (NameNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
